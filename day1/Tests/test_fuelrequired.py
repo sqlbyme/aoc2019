d826d0aff20efcd262de3481009755e09716ad17
Mike Edwards <mike@sqlby.me>
2020-03-23 17:56:02 -0700

    Got the first puzzle on day 1 working.

---
+++ day1/Tests/test_fuelrequired.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class FuelTestCase(unittest.TestCase):
+    def test_calculate_required(self):
+        from day1.fuelrequired import calculate_required
+        expected = 2
+        given_mass = 12
+        self.assertEqual(expected, calculate_required(given_mass))
+
+    def test_calculate_required_large_number(self):
+        from day1.fuelrequired import calculate_required
+        expected = 33583
+        given_mass = 100756
+        self.assertEqual(expected, calculate_required(given_mass))
+
+    def test_calc_total_fuel(self):
+        from day1.fuelrequired import calc_total_fuel
+        expected = 3362507
+        self.assertEqual(expected, calc_total_fuel(), "Not equal.")
+
+if __name__ == '__main__':
+    unittest.main()
